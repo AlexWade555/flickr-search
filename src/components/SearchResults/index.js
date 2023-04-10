38a8f7bce35a743989002c1b2d5905c2af226967
Alex Wade <alexjohnwade@gmail.com>
2023-04-10 15:08:51 +1000

    testing something

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom"
 
+
 const SearchResults = () => {
   const params = useParams()
   return (
-    <p>Search results for {params.queryText}</p>
+    <>
+      <p>Search results for {params.queryText}</p>
+      <p>"According to &quot;Star Wars&quot; lore, which planet does Obi-Wan Kenobi come from?"</p>
+    </>
   )
 }
 
